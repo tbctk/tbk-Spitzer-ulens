7de78def7f9a9a80b091ba8d9080cfbffc8c656f
Tarik Bouchoutrouch-Ku <tarik.bouchoutrouch-ku@mail.mcgill.ca>
2022-05-03 15:13:59 -0400

    tryna fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+autodoc_mock_imports = ["numpy","os","pickle","astropy","emcee","time","tqdm","VBBinaryLensing","MulensModel","scipy","matplotlib"]
+
 
 # -- Options for HTML output -------------------------------------------------
 
