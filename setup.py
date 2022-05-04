24b7a1d13fb3f2c838b3a198c77bc67554e552d1
Tarik Bouchoutrouch-Ku <tarik.bouchoutrouch-ku@mail.mcgill.ca>
2022-05-04 11:04:52 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+
+setup(
+    name='Spitzer-ulens',
+    version='0.1',
+    packages=['Spitzer_ulens'],
+    long_description=open('README.md','r').read(),
+    long_description_content_type = "text/markdown",
+)
